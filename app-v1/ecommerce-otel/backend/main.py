9f2d5b3c309103c11508748c7a21c9c8c422f2fe
tabish-605 <168262315+tabish-605@users.noreply.github.com>
2025-07-25 18:53:57 +0530

    Update main.py

--- app-v1/ecommerce-otel/backend/main.py
+++ app-v1/ecommerce-otel/backend/main.py
@@ -30,6 +30,7 @@ FastAPIInstrumentor.instrument_app(app)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
